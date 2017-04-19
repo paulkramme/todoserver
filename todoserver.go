89807f996c9a6e98193f9eb661a1c2f8eee558b8
Paul Kramme <pjkramme@gmail.com>
2017-04-20 01:36:41 +0200

    Add apihandler apihandler prints and returns send message

--- todoserver.go
+++ todoserver.go
@@ -2,18 +2,48 @@ package main
 
 import "fmt"
 import "net/http"
+import "io/ioutil"
+import "encoding/json"
 
 type Site struct {
 	Title string
-	Body []byte
+	Body  []byte
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello World")
+type object struct {
+	checkbox bool
+	desc     string
+}
+
+type response struct {
+	Title         string
+	Desc          string
+	Author        string
+	listofobjects []object
+}
+
+func fromjson(src string, v interface{}) error {
+	return json.Unmarshal([]byte(src), v)
+}
+
+func apihandler(w http.ResponseWriter, r *http.Request) {
+	defer r.Body.Close()
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Fprintf(w, "RECEIVED POST\n%s\n", body)
+	fmt.Println(string(body))
+}
+
+func roothandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello Bendix")
 }
 
 func main() {
 	fmt.Println("TODO SERVER\nCopyright by Paul Kramme 2017")
-	http.HandleFunc("/api", handler)
-	http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/api", apihandler)
+	http.HandleFunc("/", roothandler)
+	http.ListenAndServe(":80", nil)
 }
