6a446ed7a182fa04f8cfb830a747ef6ff1ac05a7
Paul Kramme <pjkramme@gmail.com>
2017-04-22 23:50:10 +0200

    Add debug comments

--- todoserver.go
+++ todoserver.go
@@ -6,15 +6,18 @@ import "io/ioutil"
 import "encoding/json"
 import "flag"
 import "database/sql"
-import "github.com/go-sql-driver/mysql"
+import _ "github.com/go-sql-driver/mysql"
 
-var iterator int
 var info bool
 
 type config struct {
-	Site_prefix     string
-	Port            int
-	Sql_server_link string
+	Api_site_prefix string
+	Listen           string
+	Info_printing   bool
+	Sql_server      string
+	Sql_user        string
+	Sql_password    string
+	Sql_port        int
 }
 
 type Object struct {
@@ -54,41 +57,70 @@ func tojson(v interface{}) ([]byte, error) {
 
 func userhandler(w http.ResponseWriter, r *http.Request) {
 	defer r.Body.Close()
-
-}
-
-func apihandler(w http.ResponseWriter, r *http.Request) {
-	//defer r.Body.Close()
-	body, err := ioutil.ReadAll(r.Body)
-	//fmt.Println("RAW_RESPONSE", string(body), "\n")
-	if err != nil {
-		fmt.Println(err)
-		fmt.Fprintf(w, "400 %s", err)
-		return
-	}
-	var resp response
-	err = fromjson(string(body), &resp)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	fmt.Fprintf(w, "200")
-	
-	if info == true {
-		iterator++
-		fmt.Println(iterator)
-		resp.printinfo()
-	}
-	r.Body.Close()
 }
 
 func main() {
 	fmt.Println("TODO SERVER\nCopyright by Paul Kramme 2017")
 
+	var iterator int
+
+	configfile, err := ioutil.ReadFile("./config.json")
+	var conf config
+	if err != nil {
+		fmt.Println("No config.json found in current directory, expecting arguments.")
+	} else {
+		err = nil
+		err = fromjson(string(configfile), &conf)
+		if err != nil {
+			panic(err)
+		}
+	}
+
 	infoprinting := flag.Bool("info", false, "Printing incoming api usage and number of connections")
 	flag.Parse()
-	info = *infoprinting
 
-	http.HandleFunc("/api", apihandler)
-	http.ListenAndServe(":80", nil)
+	fmt.Print("Connecting to database: ")
+	db_string := fmt.Sprintf("%s:%s@tcp(%s:%d)/todo", conf.Sql_user, conf.Sql_password, conf.Sql_server, conf.Sql_port)
+	db, err := sql.Open("mysql", db_string)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+	fmt.Print("success\n")
+	fmt.Print("Testing database connection: ")
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Print("success\n")
+
+	http.HandleFunc("/api", func(w http.ResponseWriter, r *http.Request) {
+		defer r.Body.Close()
+		body, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			fmt.Println(err)
+			fmt.Fprintf(w, "400 %s", err)
+			return
+		}
+		var resp response
+		err = fromjson(string(body), &resp)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		// Get user
+		// Compare user with post author
+		// post post to database
+
+		fmt.Fprintf(w, "200")
+
+		if *infoprinting == true {
+			iterator++
+			fmt.Println(iterator)
+			resp.printinfo()
+		}
+	})
+	fmt.Println("Initialization complete.")
+	http.ListenAndServe(conf.Listen, nil)
 }
